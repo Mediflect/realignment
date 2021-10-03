32de9cac2a32394615729cc11f9038912c5e6334
Jake Shields <rachiron.js@gmail.com>
2021-10-03 00:38:28 -0700

    Updated all the minigames with non-jank sprites, changed good color from green to blue to accomodate color blindness

---
+++ Assets/Scripts/CustomSlider.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomSlider : MonoBehaviour
+{
+    public Transform indicatorTransform;
+    public Transform minTransform;
+    public Transform maxTransform;
+
+    public float value;
+}

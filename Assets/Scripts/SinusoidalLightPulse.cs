fab3e7303d78f8d36a7b838c3f273cbd243c9d27
Jake Shields <rachiron.js@gmail.com>
2021-10-02 20:25:09 -0700

    Added a little light pulse to the player

---
+++ Assets/Scripts/SinusoidalLightPulse.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class SinusoidalLightPulse : MonoBehaviour
+{
+    public Light2D lightSource;
+    public float period = 2f;
+    public float amplitude = 0.2f;
+
+    private float baseIntensity = 0f;
+    private float timer = 0f;
+
+    private void OnEnable()
+    {
+        baseIntensity = lightSource.intensity;
+    }
+
+    private void OnDisable()
+    {
+        lightSource.intensity = baseIntensity;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > period)
+        {
+            timer -= period;
+        }
+        float t = Mathf.InverseLerp(0, period, timer);
+        float sinPos = Mathf.Lerp(0, Mathf.PI * 2f, t);
+        float intensity = baseIntensity + Mathf.Sin(sinPos) * amplitude;
+        lightSource.intensity = intensity;
+    }
+}
